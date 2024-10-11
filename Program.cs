85aee6fc1c656c92fa83f354269a9ba22d7e1cca
SiposZoltan63 <siposz@kkszki.hu>
2024-10-11 13:25:47 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -12,17 +12,53 @@ namespace minmax
 {
     internal class Program
     {
-        public static int[] tm new int[] {63.87.36.54.12.23.79}
-        static void Main(string[] args)
+        public static List<int> Lista = new List<int>();
+        public static void feltomt()
         {
-                if (tm[0] < tm[1]  tm[2] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[0] + "a legkisebb szám a listában");
-                if (tm[1] < tm[0] as tm[2] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[1] + "a legkisebb szám a listában");
-                if (tm[2] < tm[1] as tm[0] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[2] + "a legkisebb szám a listában");
-                if (tm[3] < tm[1] as tm[2] as tm[0] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[3] + "a legkisebb szám a listában");
-                if (tm[4] < tm[1] as tm[2] as tm[3] as tm[0] as tm[5] as tm[6]) Console.WriteLine(tm[4] + "a legkisebb szám a listában");
-                if (tm[5] < tm[1] as tm[2] as tm[3] as tm[4] as tm[0] as tm[6]) Console.WriteLine(tm[5] + "a legkisebb szám a listában");
-                if (tm[6] < tm[1] as tm[2] as tm[3] as tm[4] as tm[5] as tm[0]) Console.WriteLine(tm[6] + "a legkisebb szám a listában");
+            Random rnd = new Random();
+            for (int i = 0;i < 10; i++)
+            {
+                Lista.Add(rnd.Next(1-101));
+            }
 
+            for (int i = 0;i < Lista.Count;i++)
+            {
+                Console.WriteLine(Lista[i]+",");
+            }
+
+        }
+        public static int minimum()
+        {
+            int min = lista[0];
+
+            for (int i = 0;i <Lista.Count;i++)
+            {
+                if (Lista[1] > min)
+                {
+                    min = Lista[1];
+                }
+
+            return min;
+        }
+            public static int maximum()
+            {
+                int max = lista[0];
+
+                for (int i = 0; i < Lista.Count; i++)
+                {
+                    if (Lista[1] > max)
+                    {
+                        min = Lista[1];
+                    }
+
+                    return max;
+                }
+                static void Main(string[] args)
+        {
+            feltomt();
+            Console.WriteLine();
+            Console.WriteLine(minimum());
+            Console.WriteLine(maximum());
         }
     }
 }
