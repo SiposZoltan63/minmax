9dbc16893fc2dbbc36b15257f8494f3054f2dcd1
SiposZoltan63 <siposz@kkszki.hu>
2024-10-11 12:59:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace minmax
+{
+    internal class Program
+    {
+        public static int[] tm new int[] {63.87.36.54.12.23.79}
+        static void Main(string[] args)
+        {
+                if (tm[0] < tm[1]  tm[2] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[0] + "a legkisebb szám a listában");
+                if (tm[1] < tm[0] as tm[2] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[1] + "a legkisebb szám a listában");
+                if (tm[2] < tm[1] as tm[0] as tm[3] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[2] + "a legkisebb szám a listában");
+                if (tm[3] < tm[1] as tm[2] as tm[0] as tm[4] as tm[5] as tm[6]) Console.WriteLine(tm[3] + "a legkisebb szám a listában");
+                if (tm[4] < tm[1] as tm[2] as tm[3] as tm[0] as tm[5] as tm[6]) Console.WriteLine(tm[4] + "a legkisebb szám a listában");
+                if (tm[5] < tm[1] as tm[2] as tm[3] as tm[4] as tm[0] as tm[6]) Console.WriteLine(tm[5] + "a legkisebb szám a listában");
+                if (tm[6] < tm[1] as tm[2] as tm[3] as tm[4] as tm[5] as tm[0]) Console.WriteLine(tm[6] + "a legkisebb szám a listában");
+
+        }
+    }
+}
